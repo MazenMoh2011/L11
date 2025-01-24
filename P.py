b4aed9e3f27996bf8ea3e48709e4030761fe7896
MazenMoh2011 <mazenalnagar203@gmail.com>
2025-01-24 16:14:45 +0300

    fsfgfg

---
+++ P.py
@@ -0,0 +1,7 @@
+age=float(input("what is your age?"))
+if age == str:
+    print("try your age with a decimal value please!")
+    if age == int:
+        print("try your age with a decimal value please!")
+else:
+    print("age is float. welcome!")
